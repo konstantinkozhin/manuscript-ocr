2dc991207132db16e7d2eea8a61226360e97c079
sherstpasha <sherstpasha99@gmail.com>
2025-07-12 23:54:01 +0700

    0.1.6 finaly

--- setup.py
+++ setup.py
@@ -9,26 +9,23 @@ def parse_requirements(fname="requirements.txt"):
 
 
 setup(
-    # имя для pip install
     name="manuscript-ocr",
     version="0.1.6",
     description="EAST-based OCR detector API",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="",
-    author_email="",
+    author_email="sherstpasha99@gmail.com",
     url="https://github.com/konstantinkozhin/manuscript-ocr",
     license="MIT",
-    # здесь мы указываем, что пакеты лежат в папке src
     package_dir={"": "src"},
-    # setuptools найдёт все папки внутри src, которые содержат __init__.py
     packages=find_packages(where="src"),
     python_requires=">=3.9",
     install_requires=parse_requirements(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    include_package_data=True,
 )
